e79f42f643c5a63a7793db8d26de1f53f96c2bf3
codingwithculp <74019781+codingwithculp@users.noreply.github.com>
2026-04-03 12:19:33 -0500

    Fixed LIGHT_BLUE being defined twice

--- p1_hello.asm
+++ p1_hello.asm
@@ -1,26 +1,41 @@
 ; -------------------------------------------------
-; C64 HELLO WORLD via direct screen writes
+; C64 HELLO WORLD the harttd way...via direct screen
+; writes
 ; Screen clear via KERNAL CHROUT ($93)
 ; David Culp
 ; codingwithculp@gmail.com
 ; -------------------------------------------------
 
 ; -------------------------
-; Memory locations
+; Memory location constants
 ; -------------------------
-SCREEN_RAM  = 1024      ;1024 decimal
-COLOR_RAM   = $D800      ;55296 decimal
+SCREEN_RAM     = $0400                ;1024 decimal
+COLOR_RAM      = $D800                ;55296 decimal
 
 ; -------------------------
-; KERNAL routine
+; KERNAL routine constants
 ; -------------------------
 CHROUT      = $FFD2      ;65490 decimal 
 
 ; -------------------------
-; Colors
+; Color constants
 ; -------------------------
-WHITE       = $01        ;1 decimal
-LIGHT_BLUE  = $0E        ;14 decimal
+BLACK          = $00     ;0 decimal
+WHITE          = $01     ;1 decimal
+RED            = $02     ;2 decimal
+CYAN           = $03     ;3 decimal
+PURPLE         = $04     ;4 decimal
+GREEN          = $05     ;5 decimal
+BLUE           = $06     ;6 decimal
+YELLOW         = $07     ;7 decimal
+ORANGE         = $08     ;8 decima;
+BROWN          = $09     ;9 decimal
+LIGHT_RED      = $0A     ;10 decimal
+GREY_1         = $0B     ;11 decimal
+GREY_2         = $0C     ;12 decimal
+LIGHT_GREEN    = $0D     ;13 decimal
+LIGHT_BLUE     = $0E     ;14 decimal
+GREY_3         = $0F     ;15 decimal
 
 ; CODE BEGINS HERE!
 * = $033C      ;decimal 828, start of cassette buffer
